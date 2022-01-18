29dce527f4215dfe3a6faf72ae07d9b40f640ced
ShrirajSDSU <sjahagirdar1790@sdsu.edu>
2022-01-18 00:40:47 -0800

    added min values for quantity and unit_price

--- validators/update.js
+++ validators/update.js
@@ -5,8 +5,8 @@ const updateSchema = {
     properties: {
         name: { type: "string" },
         company: { type: "string" },
-        quantity: { type: "number" },
-        unit_price: { type: "number" },
+        quantity: { type: "number", minimum: 1 },
+        unit_price: { type: "number", minimum: 1 },
         image_url: { type: "string", format: "uri" },
         thumbnail_url: { type: "string", format: "uri" }
     },
