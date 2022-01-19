b1b5d63ecabb663bb04ce8d7758050c84ac7bfe6
ShrirajSDSU <sjahagirdar1790@sdsu.edu>
2022-01-19 00:59:05 -0800

    formated small things

--- helper/clean-deleted-table.js
+++ helper/clean-deleted-table.js
@@ -1,10 +1,8 @@
 function clean() {
+  // Check if record inserted in deleted table is older than 15 mins this cron job was run...
+  // delete the records older than 15 mins, of this cron job
 
-    // Check if record inserted in deleted table is older than 15 mins this cron job was run...
-    //delete the records older than 15 mins, of this cron job
-
-    console.log('Called at - ', Date.now())
-
+  console.log('Called at - ', Date.now());
 }
 
 module.exports = clean;
--- models/comments.js
+++ models/comments.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema(
-    {
-        comment: {
-            type: String,
-            required: true
-        },
-        itemId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Inventory'
-        },
-
+  {
+    comment: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Inventory',
+    },
+
+  },
+  {
+    timestamps: true,
+  },
 );
 const Comment = mongoose.model('Comment', commentSchema);
 
